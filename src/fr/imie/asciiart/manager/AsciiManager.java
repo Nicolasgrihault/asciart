fc70305d3c2d0909a65e9ebf6aab0130a3b82287
NicolasG <nicolas.grihault@imie.fr>
2018-10-17 16:47:53 +0200

    sta3

--- src/fr/imie/asciiart/manager/AsciiManager.java
+++ src/fr/imie/asciiart/manager/AsciiManager.java
@@ -1,33 +1,45 @@
 package fr.imie.asciiart.manager;
 
-public class AsciiManager {
-	public (String args[]) {
-        Scanner in = new Scanner(System.in);
-        int L = in.nextInt();
-        int H = in.nextInt();
-        if (in.hasNextLine()) {
-            in.nextLine();
-        }
-        String T = in.nextLine();
-        T = T.toUpperCase();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < H; i++) {
-            String ROW = in.nextLine();
-            for(char letter : T.toCharArray()){
-                if(letter >= 65 && letter <= 90){
-                    result.append(ROW.substring((letter-65)*L,(letter-65)*L+L));
-                }
-                else{
-                    result.append(ROW.substring(L*26,(L*26)+L));
-                }
-            }
-            result.append("\n");
-        }
-        
-        
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
+import java.util.Scanner;
 
-        System.out.println(result.toString());
-    }   	
+import fr.imie.asciiart.alphabets.DefaultAlphabet;
+import fr.imie.asciiart.alphabets.datas.AlphabetsDatas;
+
+public class AsciiManager {
+	public DefaultAlphabet alphabet;
+	
+	
+	public AsciiManager(DefaultAlphabet alphabet) {
+		super();
+		this.alphabet = alphabet;
+	}
+
+
+	public String[] GetAsciiTranslation(String message) {
+	        Scanner in = new Scanner(System.in);
+	        int L = this.alphabet.getW();
+	        int H = this.alphabet.getH();
+	        String T = in.nextLine();
+	        T = T.toUpperCase();
+	        StringBuilder result = new StringBuilder();
+	        for (int i = 0; i < H; i++) {
+	            String ROW = this.alphabet.toString();
+	            for(char letter : T.toCharArray()){
+	                if(letter >= 65 && letter <= 90){
+	                    result.append(ROW.substring((letter-65)*L,(letter-65)*L+L));
+	                }
+	                else{
+	                	result.append(ROW.substring(L*26,(L*26)+L));
+	                }
+	            }
+	            result.append("\n");
+	        }
+	        
+	        
+	        // Write an action using System.out.println()
+	        // To debug: System.err.println("Debug messages...");
+
+	        System.out.println(result.toString());
+			return null;
+	    } 
 }
