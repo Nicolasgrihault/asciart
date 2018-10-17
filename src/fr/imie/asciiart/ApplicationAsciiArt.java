59810c4006db45de621103579fe77d97ed2ba9e8
NicolasG <nicolas.grihault@imie.fr>
2018-10-17 09:52:59 +0200

    start

---
+++ src/fr/imie/asciiart/ApplicationAsciiArt.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.imie.asciiart;
+
+/**
+ * @author NicolasG
+ *
+ */
+public class ApplicationAsciiArt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
